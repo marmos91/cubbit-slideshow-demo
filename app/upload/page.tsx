85fd37f6b08772a1677f5284173a2681722509ea
marmos91 <m.marmos@gmail.com>
2025-03-15 23:41:22 +0100

    form style ready

--- app/upload/page.tsx
+++ app/upload/page.tsx
@@ -1,9 +1,16 @@
 import UploadForm from '../components/UploadForm';
 import styles from './page.module.css';
+import { Titillium_Web } from 'next/font/google';
+
+const titillium = Titillium_Web({
+    weight: '400',
+    subsets: ['latin'],
+    style: 'normal',
+});
 
 const UploadPage = () => {
     return (
-        <main className={styles.uploadPage}>
+        <main className={[styles.uploadPage, titillium.className].join(' ')}>
             <div className={styles.logo}>
                 <img src="/cubbit.png" alt="Cubbit logo" />
                 <img src="/elemento.png" alt="Cubbit logo" />
