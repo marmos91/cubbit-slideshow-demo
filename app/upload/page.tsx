93f9719dc8099367d475a37c99342a93dddcd443
marmos91 <m.marmos@gmail.com>
2025-03-16 02:07:19 +0100

    improve logos

--- app/upload/page.tsx
+++ app/upload/page.tsx
@@ -1,5 +1,6 @@
 import UploadForm from '../components/UploadForm';
 import styles from './page.module.css';
+import Image from 'next/image';
 import { Titillium_Web } from 'next/font/google';
 
 const titillium = Titillium_Web({
@@ -12,8 +13,8 @@ const UploadPage = () => {
     return (
         <main className={[styles.uploadPage, titillium.className].join(' ')}>
             <div className={styles.logo}>
-                <img src="/cubbit.png" alt="Cubbit logo" />
-                <img src="/elemento.png" alt="Elemento logo" />
+                <Image src="/cubbit.png" alt="Cubbit logo" width={60} height={80} priority />
+                <Image src="/elemento.png" alt="Elemento logo" width={80} height={80} priority />
             </div>
             <h1 className={styles.h1}>Upload a photo now to celebrate CloudFest</h1>
             <UploadForm />
