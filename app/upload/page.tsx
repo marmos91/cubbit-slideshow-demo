890b58d29a1b6cd79d42438a61ebe0076829d2ea
marmos91 <m.marmos@gmail.com>
2025-03-18 10:13:13 +0100

    refactor upload form

--- app/upload/page.tsx
+++ app/upload/page.tsx
@@ -1,22 +1,24 @@
-import UploadForm from '../components/UploadForm';
+import UploadForm from '../components/upload/UploadForm';
 import styles from './page.module.css';
 import Image from 'next/image';
-import { Titillium_Web } from 'next/font/google';
+import { Orbitron } from 'next/font/google';
 
-const titillium = Titillium_Web({
+const orbitron = Orbitron({
     weight: '400',
     subsets: ['latin'],
     style: 'normal',
 });
 
+const h1Copy = 'Upload a photo to Cubbit DS3 Powered by Elemento';
+
 const UploadPage = () => {
     return (
-        <main className={[styles.uploadPage, titillium.className].join(' ')}>
+        <main className={[styles.uploadPage, orbitron.className].join(' ')}>
             <div className={styles.logo}>
                 <Image src="/cubbit.png" alt="Cubbit logo" width={60} height={80} priority />
                 <Image src="/elemento.png" alt="Elemento logo" width={80} height={80} priority />
             </div>
-            <h1 className={styles.h1}>Upload a photo now to celebrate CloudFest</h1>
+            <h1 className={styles.h1}>{h1Copy}</h1>
             <UploadForm />
         </main>
     );
